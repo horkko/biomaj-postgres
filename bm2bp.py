61a41a65162d10f757e4aa937f69822d08ba63c3
horkko <horkko@gmail.com>
2016-04-22 13:59:57 +0200

    Remove hardcoded connection values

--- bm2bp.py
+++ bm2bp.py
@@ -5,7 +5,7 @@ import psycopg2
 from psycopg2 import OperationalError, DatabaseError, IntegrityError
 import json
 from biomaj.config import BiomajConfig
-
+import os
 """
 This small script is a test script to transfert data from a Biomaj MongoDB
 database into a PostgreSQL data using Jsonb data typexs
@@ -20,16 +20,16 @@ if __name__ == '__main__':
     banks = []
 
     insert_query = "INSERT INTO bank(data) VALUES "
-    #for bank in m_bank.find({'name': {'$in': ['alu', 'btaurus']}}, {'_id': 0}):
     for bank in m_bank.find({}, {'_id': 0}):
-        # banks.append(json.dumps(bank))
         insert_query += "('%s')," % json.dumps(bank)
     insert_query = insert_query.strip(',')
 
     # In case we empty the databble
 
     try:
-        pc = psycopg2.connect("dbname=biomaj user=tuco password=tucobell port=5432")
+        pc = psycopg2.connect("dbname=biomaj user=%s password=%s port=%s" %
+                              (os.getenv('PGUSER'), os.getenv('PGPASSWORD'),
+                               os.getenv('PGPORT')))
         cursor = pc.cursor()
         cursor.execute(insert_query)
         pc.commit()
